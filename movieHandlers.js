4458c9ceac92c53500577e9cc1f27fe84317a336
paul battault <paulbattault@gmail.com>
2022-12-12 11:51:02 +0100

    exercice 6 ok

--- movieHandlers.js
+++ movieHandlers.js
@@ -30,8 +30,24 @@ const movies = [
 const database = require("./database");
 
 const getMovies = (req, res) => {
+  let sql = "select * from movies";
+  const sqlValues = [];
+
+  if (req.query.color != null) {
+    sql += " where color = ?";
+    sqlValues.push(req.query.color);
+ 
+    if (req.query.max_duration != null) {
+    sql += " and duration <= ?";
+    sqlValues.push(req.query.max_duration);
+  }
+  } else if (req.query.max_duration != null) {
+  sql += " where duration <= ?";
+  sqlValues.push(req.query.max_duration);
+}
+
   database
-    .query("select * from movies")
+    .query(sql, sqlValues)
     .then(([movies]) => {
       res.json(movies);
     })
@@ -40,6 +56,43 @@ const getMovies = (req, res) => {
       res.status(500).send("Error retrieving data from database");
     });
 };
+// const getMovies = (req, res) => {
+//   const initialSql = "select * from movies";
+//   const where = [];
+
+//   if (req.query.color != null) {
+//     where.push({
+//       column: "color",
+//       value: req.query.color,
+//       operator: "=",
+//     });
+//   }
+//   if (req.query.max_duration != null) {
+//     where.push({
+//       column: "duration",
+//       value: req.query.max_duration,
+//       operator: "<=",
+//     });
+//   }
+
+//   database
+//     .query(
+//       where.reduce(
+//         (sql, { column, operator }, index) =>
+//           `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+//         initialSql
+//       ),
+//       where.map(({ value }) => value)
+//     )
+//     .then(([movies]) => {
+//       res.json(movies);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error retrieving data from database");
+//     });
+// };
+
 const addMovie = (req, res) => {
   const { title, director, year, color, duration } = req.body;
 
@@ -100,8 +153,23 @@ const deleteMovieById = (req, res) => {
 };
 
 const getUsers = (req, res) => {
+  let sql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+ 
+    if (req.query.city != null) {
+    sql += " and city = ?";
+    sqlValues.push(req.query.city);
+  }
+  } else if (req.query.city != null) {
+  sql += " where city = ?";
+  sqlValues.push(req.query.city);
+}
   database
-    .query("select * from users")
+    .query(sql, sqlValues)
     .then(([users]) => {
       res.json(users);
     })
@@ -109,7 +177,7 @@ const getUsers = (req, res) => {
       console.error(err);
       res.status(500).send("Error retrieving data from database");
     });
-    };
+};
 
 const addUsers = (req, res) => {
   const { firstname, lastname, email, city, language } = req.body;
